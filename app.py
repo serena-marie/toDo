87331b985b33760bae2c818219a438887c9db57e
serena-marie <serena-marie@users.noreply.github.com>
2019-01-25 13:59:21 -0600

    Finished tutorial week one

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,7 +17,34 @@ class Todo(db.Model):
 # server side
 @app.route('/')
 def index():
-    return render_template('index.html')
+    incomplete = todos = Todo.query.filter_by(complete=False).all()
+    complete = Todo.query.filter_by(complete=True).all()
+
+    return render_template('index.html', todos=todos, incomplete=incomplete, complete=complete)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = Todo(text=request.form['todoitem'], complete=False)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+    # return '<h1>{}</h1>'.format(request.form['todoitem']) # now we know it was received
+
+
+@app.route('/complete/<id>')
+def complete(id):
+    # return '<h1>{}</h1>'.format(id) # confirm it receives id
+    todo = Todo.query.filter_by(id=int(id)).first()
+    todo.complete = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# Resume with checkboxes:
+# @app.route('/update', methods=['POST'])
+# def update():
+#    print(request.form)
+#    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
